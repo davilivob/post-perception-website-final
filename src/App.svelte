57e1e8138d4ef089a474f697dd9a5e1e9c636458
davilivob <davilivob@gmail.com>
2023-04-07 22:23:26 +0800

    new bg color

--- src/App.svelte
+++ src/App.svelte
@@ -50,12 +50,17 @@
 </script>
 
 <TailwindCSS/>
-<div class="bg-gradient-to-br from-blue-900/80 via-black/100 to-cyan-800/50 w-screen h-screen fixed z-[-3]"></div>
-<div class="bg-gradient-to-bl from-black via-pink-900/10 to-black w-screen h-screen fixed z-[-1] backdrop-blur-2xl"></div>
+<div class="bg-gradient-to-tl from-blue-900 via-emerald-900 to-fuchsia-900 w-screen h-screen fixed z-[-3]"></div>
+<div class="bg-gradient-to-bl from-lime-900 via-indigo-900 to-purple w-screen h-screen fixed z-[-1] backdrop-blur-2xl"></div>
+<div class="bg-black/70 w-screen h-screen fixed z-[-1] backdrop-blur-2xl"></div>
+
+<div class="w-[10vw] h-[10vw] fixed rounded-full ml-[60vw] mt-[60vh] blur-3xl bg-blue-900/40 z-[-1]"></div>
+<div class="w-[20vw] h-[20vw] fixed rounded-full ml-[70vw] mt-[50vh] blur-3xl bg-violet-900/40 z-[-1]"></div>
+<div class="w-[40vw] h-[40vw] fixed rounded-full ml-[80vw] mt-[35vh] blur-3xl bg-red-900/40 z-[-1]"></div>
 <main>
     <Cursor/>
     <Header language="{language}" page="{current_page}"/>
-    <div class="py-8">
+    <div class="">
         <Router {routes}/>
     </div>
 </main>
